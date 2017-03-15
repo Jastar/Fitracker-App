eb6bc63428be4bd5adde9129d10251bd72065910
Angeal <rozenkreuz64@gmail.com>
2017-03-15 14:48:10 +0200

    adding effects

--- js/common.js
+++ js/common.js
@@ -16,4 +16,23 @@ $(document).ready(function(){
    $("nav ul.socialgroup li.share").mouseleave(function(){
    		$("ul.submenu").fadeOut();
    })
+
+   window.onscroll = function() {
+        if (window.pageYOffset > 500) {
+            $("section#gallery h2").addClass("animated bounceIn");
+            $("section#gallery h4").addClass("animated bounceIn");
+            $("section#gallery hr").addClass("animated fadeIn");
+            $("section#gallery ul.groups").addClass("animated bounceIn");
+        } 
+        if (window.pageYOffset > 1000) {
+            $("section#gallery img[src='images/gallery1.gif']").addClass("animated zoomIn");
+            $("section#gallery img[src='images/gallery4.gif']").addClass("animated zoomIn");
+
+            $("section#gallery img[src='images/gallery2.gif']").addClass("animated zoomIn");
+            $("section#gallery img[src='images/gallery5.gif']").addClass("animated zoomIn");
+
+            $("section#gallery img[src='images/gallery3.gif']").addClass("animated zoomIn");
+            $("section#gallery img[src='images/gallery6.gif']").addClass("animated zoomIn");
+        }
+    };
 });
